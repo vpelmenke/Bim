9e8182641903cb35884e822670f32f25261e211d
Sergey <cergeypost.gmail.com>
2020-06-08 23:39:28 +0300

    Добавьте файлы проекта.

---
+++ Bim/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+
+namespace Bim
+{
+    class MainViewModel:INotifyPropertyChanged
+    {
+        public List<Person> persons = new List<Person>();
+        public MainViewModel()
+        {
+            using (StreamReader r = new StreamReader("big_data_persons.json"))
+            {
+                string input = r.ReadToEnd();
+                persons = JsonConvert.DeserializeObject<List<Person>>(input);
+            }
+        }
+
+        private Person currentPerson;
+        public Person CurrentPerson
+        {
+            get => currentPerson;
+            set
+            {
+                currentPerson = value;
+                OnPropertyChanged("CurrentPerson");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Bim/Person.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Bim
+{
+    class Person : INotifyPropertyChanged
+    {
+        private string id;
+        private string name;
+        private string age;
+        public string ID
+        {
+            get => id;
+            set
+            {
+                id = value;
+                OnPropertyChanged("ID");
+            }
+        }
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Age
+        {
+            get => age;
+            set
+            {
+                age = value;
+                OnPropertyChanged("Age");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
