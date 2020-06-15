cce52c06220e323b77e402556724e25433b1b12a
Sergey <cergeypost.gmail.com>
2020-06-15 10:30:52 +0300

    Added Task 3.

--- Bim/MainViewModel.cs
+++ Bim/MainViewModel.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Windows.Input;
 using Newtonsoft.Json;
 
 namespace Bim
@@ -13,32 +12,18 @@ namespace Bim
     {
         public ObservableCollection<Person> Persons { get; set; }
         public ObservableCollection<Contact> Contacts { get; set; }
-        private int page = 2;
-
+        private int page = 0;
         public MainViewModel()
         {
             Persons = new ObservableCollection<Person>();
             Contacts = new ObservableCollection<Contact>();
-
-            using (StreamReader r = new StreamReader("big_data_persons.json"))
-            {
-                string input = r.ReadToEnd();
-                var t = JsonConvert.DeserializeObject<ObservableCollection<Person>>(input);
-                Persons = Paging(10, t);
-            }
-            using (StreamReader r = new StreamReader("big_data_contracts.json"))
-            {
-                string input = r.ReadToEnd();
-                var t = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(input);
-                Contacts = new ObservableCollection<Contact>(t.Take(100));
-            }
-
         }
 
         private Command forwardCommand;
         private Command backCommand;
-        private Command goCommand;
-        public Command ForwardCommand
+        private Command averageCommand;
+        private Command showCommand;
+        public Command ForwardCommand // Переход на следующую страницу
         {
             get
             {
@@ -46,31 +31,162 @@ namespace Bim
                        (forwardCommand = new Command(obj =>
                        {
                            Persons = new ObservableCollection<Person>();
-
+                           Contacts = new ObservableCollection<Contact>();
+                           page++;
                            using (StreamReader r = new StreamReader("big_data_persons.json"))
                            {
                                string input = r.ReadToEnd();
-                               var t = JsonConvert.DeserializeObject<ObservableCollection<Person>>(input);
-                               page++;
-                               Persons = Paging(10, t);
+                               var a = JsonConvert.DeserializeObject<ObservableCollection<Person>>(input);
+                               Persons = PagingForPersons(10, a);
+                               OnPropertyChanged("Persons");
+                           }
+                           using (StreamReader r = new StreamReader("big_data_contracts.json"))
+                           {
+                               string input = r.ReadToEnd();
+                               var b = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(input);
+                               Contacts = PagingForContacts(10, b);
+                               OnPropertyChanged("Contacts");
                            }
                        }));
             }
         }
-        public Command BackCommand
+        public Command BackCommand // Переход на прошлую страницу
         {
             get
             {
                 return backCommand ??
                        (backCommand = new Command(obj =>
                        {
-                           page--;
+                           Persons = new ObservableCollection<Person>();
+                           Contacts = new ObservableCollection<Contact>();
+                           if (page > 0)page--;
+                           using (StreamReader r = new StreamReader("big_data_persons.json"))
+                           {
+                               string input = r.ReadToEnd();
+                               var a = JsonConvert.DeserializeObject<ObservableCollection<Person>>(input);
+                               Persons = PagingForPersons(10, a);
+                               OnPropertyChanged("Persons");
+                           }
+
+                           using (StreamReader r = new StreamReader("big_data_contracts.json"))
+                           {
+                               string input = r.ReadToEnd();
+                               var b = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(input);
+                               Contacts = PagingForContacts(10, b);
+                               OnPropertyChanged("Contacts");
+                           }
+                       }));
+            }
+        }
+
+        public Command AverageCommand // Расчет среднего возраста
+        {
+            get
+            {
+                return averageCommand ?? 
+                       (averageCommand = new Command(obj =>
+                       {
+                           var count = 0;
+                           var age = 0;
+                           using (StreamReader r = new StreamReader("big_data_persons.json"))
+                           {
+                               string input = r.ReadToEnd();
+                               var a = JsonConvert.DeserializeObject<ObservableCollection<Person>>(input);
+                               foreach (var VARIABLE in a)
+                               {
+                                   var name = VARIABLE.Name.Split(' ');
+                                   if (name[1] == UserName)
+                                   {
+                                       count++;
+                                       age += Convert.ToInt32(VARIABLE.Age);
+                                   }
+                               }
+
+                               if (count > 0)
+                               {
+                                   var result = age / count;
+                                   AverAge = result.ToString();
+                                   OnPropertyChanged("AverAge");
+                               }
+                           }
+                       }));
+            }
+        }
+
+        public Command ShowCommand // Отображение контактов в указанном интервале, которые длились > 10 минут.
+        {
+            get
+            {
+                return showCommand ??
+                       (showCommand = new Command(obj =>
+                       {
+                           using (StreamReader r = new StreamReader("big_data_contracts.json"))
+                           {
+                               string input = r.ReadToEnd();
+                               var b = JsonConvert.DeserializeObject<ObservableCollection<Contact>>(input);
+                               Contacts.Clear();
+                               foreach (var VARIABLE in b)
+                               {
+                                   var dateFrom = Convert.ToDateTime(VARIABLE.From);
+                                   var dateTo = Convert.ToDateTime(VARIABLE.To);
+                                   if (dateFrom >= DateIn && dateTo <= DateOut)
+                                       if (dateTo.Subtract(dateFrom).Minutes > 10)
+                                           Contacts.Add(VARIABLE);
+
+                                   if(Contacts.Count == 10) break; // Здесь нужно написать нормальный обработчик вывода.
+                               }
+                               OnPropertyChanged("Contacts");
+                           }
                        }));
             }
         }
 
         private Person currentPerson;
         private Contact currentContact;
+        private string userName;
+        private string averAge;
+        private DateTime dateIn = DateTime.Today;
+        private DateTime dateOut = DateTime.Today;
+
+        public DateTime DateIn
+        {
+            get => dateIn;
+            set
+            {
+                dateIn = value;
+                OnPropertyChanged("DateIn");
+            }
+        }
+        public DateTime DateOut
+        {
+            get => dateOut;
+            set
+            {
+                dateOut = value;
+                OnPropertyChanged("DateOut");
+            }
+        }
+
+        public string UserName
+        {
+            get => userName;
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        public string AverAge
+        {
+            get => averAge;
+            set
+            {
+                averAge = value;
+                OnPropertyChanged("AverAge");
+            }
+        }
+
         public Person CurrentPerson
         {
             get => currentPerson;
@@ -92,11 +208,18 @@ namespace Bim
         }
 
 
-        private ObservableCollection<Person> Paging(int number, ObservableCollection<Person> t)
+        private ObservableCollection<Person> PagingForPersons(int number, ObservableCollection<Person> t)
         {
             ObservableCollection<Person> persons = new ObservableCollection<Person>();
             persons = new ObservableCollection<Person>(t.Take(number * page).Skip(number * (page - 1)));
             return persons;
+        }
+
+        private ObservableCollection<Contact> PagingForContacts(int number, ObservableCollection<Contact> t)
+        {
+            ObservableCollection<Contact> contacts = new ObservableCollection<Contact>();
+            contacts = new ObservableCollection<Contact>(t.Take(number * page).Skip(number * (page - 1)));
+            return contacts;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
